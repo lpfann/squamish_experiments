ef2d6f0585275256bba6fbd30c1adb5b062aa0a4
Lukas Pfannschmidt <lukas@lpfann.me>
2020-04-13 15:48:03 +0200

    Add analysis per feature class

--- metrics.py
+++ metrics.py
@@ -25,6 +25,21 @@ def get_truth_new(params):
     truth = [True] * (strong + weak + repeated) + [False] * irrel
     return truth
 
+def get_truthAR(params):
+    strong=params["n_strel"]
+    weak=params["n_redundant"]
+    irrel = params["n_features"] - strong - weak
+    truth = [2] * strong + [1]*weak + [0] * irrel
+    return truth
+    
+# We only take truth if relevance type (1 or 2) 
+def get_truth_onetype(params, reltype):
+    truth = get_truthAR(params)
+    truth = np.array(truth)
+    if reltype==0:
+        raise Exception("0 makes no sense")
+    return truth == reltype
+
 def get_scores_for_set(truth, fset, scores=[precision_score,recall_score,f1_score], AR=True):
     if AR:
         if 2 in fset:
--- run_NL_comparison.py
+++ run_NL_comparison.py
@@ -13,6 +13,7 @@ import pandas as pd
 import sklearn as sk
 import sklearn.datasets as data
 import sklearn.metrics
+from sklearn.metrics import precision_score, roc_auc_score, recall_score, f1_score
 from sklearn.preprocessing import scale
 sys.path.append("./runner/")
 import experiment_pipeline
@@ -36,11 +37,13 @@ default_params = {
     "linear":False
 }
 datasets = {
-    "NL 1": {"n_features": 50, "n_strel": 10, "n_redundant": 0,},
-    "NL 2": {"n_features": 50, "n_strel": 0, "n_redundant": 10,},
+    "NL 1": {"n_features": 20, "n_strel": 10, "n_redundant": 0,},
+    "NL 2": {"n_features": 20, "n_strel": 4, "n_redundant": 10,},
     "NL 3": {"n_features": 50, "n_strel": 10, "n_redundant": 10,},
-    "NL 4": {"n_features": 200, "n_strel": 10, "n_redundant": 10},
+    "NL 4": {"n_features": 80, "n_strel": 10, "n_redundant": 10},
 }
+
+ARmodels = ["FRI", "SQ"] 
 
 @dataclasses.dataclass
 class Result:
@@ -97,7 +100,6 @@ def run_experiment(state = np.random.RandomState(123), n_jobs = -1,   repeats = 
                 print(r)
                 model.fit(X, y)
                 #train_score = model.score(X, y)
-                prediction = model.predict(X)
                 # Cast -1,1 encoding to 0,1 encoding of classes (in case of fri)
                 accuracy = model.score(X,y)
                 features = model.support()
@@ -113,14 +115,52 @@ def run_experiment(state = np.random.RandomState(123), n_jobs = -1,   repeats = 
 
     return exp
 
+def get_Accuracy_per_Relevance_Class(table):
+    onlyAR = table[table.model.isin(ARmodels)]
+    onlyAR = onlyAR.set_index(["dataset","model"])
+    onlyAR_features = onlyAR[["features"]]
+    
+    def rowfunc(row, reltype, scorefunc):
+        # Functhon which applies score function on feature list and compares with ground truth
+        data = row.name[0]
+        features = row["features"]
+        pred = features == reltype
+
+        truth = metrics.get_truth_onetype(datasets[data], reltype)
+        if sum(truth)==0:
+            return np.nan
+        score  = scorefunc(truth, pred,zero_division=1)
+        
+        #if score == 0:
+        #    score = np.nan
+        return score
+
+    weakly_precision = onlyAR_features.apply(rowfunc, axis=1, args=[1,precision_score])
+    weakly_recall = onlyAR_features.apply(rowfunc, axis=1, args=[1,recall_score])
+    weakly = pd.concat([weakly_precision,weakly_recall],axis=1).rename(columns={0:"precision",1:"recall"})
 
 
+    strongly_precision = onlyAR_features.apply(rowfunc, axis=1, args=[2,precision_score])
+    strongly_recall = onlyAR_features.apply(rowfunc, axis=1, args=[2,recall_score])
+    strongly = pd.concat([strongly_precision,strongly_recall],axis=1).rename(columns={0:"precision",1:"recall"})
+
+    combined = pd.concat([weakly,strongly],axis=1,keys=["Weakly","Strongly"]).round(decimals=2)
+
+    mean_datamodel = combined.groupby(["dataset","model"]).mean()
+
+    mean_model = mean_datamodel.groupby(["model"]).mean()
+
+    return combined, mean_model
 
 def analyze(exp=None):
-    exp = exp.set_index(["dataset", "model"])
 
+    #
+    #
+    # Accuracy over all features
+    #
+    exp_with_index = exp.set_index(["dataset", "model"])
     # Take mean
-    table = exp.groupby(["dataset", "model"]).mean()
+    table = exp_with_index.groupby(["dataset", "model"]).mean()
     # Round
     table = table.round(decimals=2)
     # Reformat
@@ -128,7 +168,7 @@ def analyze(exp=None):
     print_df_astable(table, "per_dataset", folder="NL_toy_benchmarks")
 
     # Take mean over all data
-    overallmean = exp.groupby(["model"]).mean()
+    overallmean = exp_with_index.groupby(["model"]).mean()
     # Round
     overallmean = overallmean.round(decimals=2)
     print_df_astable(overallmean, "mean_stats", folder="NL_toy_benchmarks")
@@ -138,7 +178,15 @@ def analyze(exp=None):
     preset = preset.astype(int)
     preset.index.names = ["Set"]
     print_df_astable(preset.T, "presets", folder="NL_toy_benchmarks")
-    return table, overallmean
+
+    #
+    #
+    # Analyse Feature Classes Accuracy
+    #
+    fclasses_combined, fclasses_meanmodel = get_Accuracy_per_Relevance_Class(exp)
+    print_df_astable(fclasses_combined, "accuracy_per_fclass", folder="NL_toy_benchmarks")
+    print_df_astable(fclasses_meanmodel, "accuracy_per_fclass_mean", folder="NL_toy_benchmarks")
+
 
 @click.command()
 @click.option("--recompute", default=False, is_flag=True)
@@ -149,14 +197,13 @@ def run(recompute, n_jobs, seed, repeats):
     if not recompute:
         try:
             exp = pd.read_pickle(TMP / (EXP_FILE + ".pickle"))
-            return exp
         except NameError:
             pass
         except FileNotFoundError:
             pass
-    exp = run_experiment(state = np.random.RandomState(seed), n_jobs = n_jobs,   repeats = repeats)
-    table, overallmean = analyze(exp)
-
+    if exp is None:
+        exp = run_experiment(state = np.random.RandomState(seed), n_jobs = n_jobs,   repeats = repeats)
+    analyze(exp)
 
 if __name__ == "__main__":
     run()
